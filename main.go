20e9b705b4e305b02e2fff632c2b9c91df34f2d4
Brigham Thompson <brigham.thompson@gmail.com>
2018-07-23 18:59:25 -0700

    adds tokenizer and search for anchor tags

--- main.go
+++ main.go
@@ -3,13 +3,14 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
+
+	"golang.org/x/net/html"
 )
 
 func main() {
-	geturl := flag.String("url", "http://www.google.com", "get request destination")
+	geturl := flag.String("url", "http://www.google.com/robots.txt", "get request destination")
 	flag.Parse()
 
 	resp, err := http.Get(*geturl)
@@ -18,10 +19,27 @@ func main() {
 	}
 	defer resp.Body.Close()
 
-	respbody, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal(err)
+	// respbody, err := ioutil.ReadAll(resp.Body)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	//
+	// fmt.Printf("%s", respbody)
+
+	z := html.NewTokenizer(resp.Body)
+	for {
+		tokType := z.Next()
+
+		switch {
+		case tokType == html.ErrorToken:
+			return
+		case tokType == html.StartTagToken:
+			if tok := z.Token(); tok.Data == "a" {
+				fmt.Println("Link found.")
+			}
+		default:
+			fmt.Println("No link found.")
+		}
 	}
 
-	fmt.Printf("%s", respbody)
 }
